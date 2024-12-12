5dcab3c4cf81c344a3ea3699c00a44fc114d261e
Séverin Lemaignan <severin.lemaignan@pal-robotics.com>
2024-12-12 12:57:16 +0100

    add proprietary license

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,11 @@
+// Copyright 2024 PAL Robotics S.L.
+//
+// Unauthorized copying of this file, via any medium is strictly prohibited,
+// unless it was supplied under the terms of a license agreement or
+// nondisclosure agreement with PAL Robotics SL. In this case it may not be
+// copied or disclosed except in accordance with the terms of that agreement.
+
+
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
